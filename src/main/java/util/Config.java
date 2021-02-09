1ffe1ac486ea2741929fa81934dd871789d2711b
valentyn <v.kostromin@kitsoft.kiev.ua>
2021-02-09 21:25:24 +0200

    add Config.java and config files example;

---
+++ src/main/java/util/Config.java
@@ -0,0 +1,62 @@
+package util;
+
+import lombok.Getter;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Class for loading configuration.
+ * Example of this file in src/main/resources/config.xml
+ */
+public abstract class Config {
+
+    private final static Logger LOGGER = Logger.getLogger(Config.class);
+
+    private final static String PATH_TO_CONFIG_FILE = "/config.xml";
+
+    private static final Properties props = new Properties();
+
+    @Getter
+    private static String jenkinsUrl;
+
+    @Getter
+    private static String jenkinsBasicAuth;
+
+    @Getter
+    private static boolean hasJenkins = false;
+
+    @Getter
+    private static Set<String> jenkinsPackages;
+
+    @Getter
+    private static Set<String> jenkinsJobPaths;
+
+    static {
+        try {
+            props.loadFromXML(Config.class.getResourceAsStream(PATH_TO_CONFIG_FILE));
+            if (props.containsKey("jenkinsUrl")) {
+                hasJenkins = true;
+                jenkinsUrl = getProperty("jenkinsUrl");
+                jenkinsBasicAuth = getProperty("jenkinsBasicAuth");
+                jenkinsPackages = Arrays.stream(getProperty("jPackageNames").split(",")).map(String::trim)
+                        .collect(Collectors.toSet());
+                jenkinsJobPaths = Arrays.stream(getProperty("jPaths").split(",")).map(String::trim)
+                        .collect(Collectors.toSet());
+            }
+        } catch (IOException e) {
+            LOGGER.error("Exception when load configuration with path " + PATH_TO_CONFIG_FILE, e);
+        }
+    }
+
+    public static String getProperty(String name) {
+        if (!props.containsKey(name)) {
+            LOGGER.error("Config option with name - " + name + " does not exists.");
+        }
+        return props.getProperty(name);
+    }
+}
